eaec695d103f029e2372cbd19f16471835083b5d
erikemb <115750399+erikemb@users.noreply.github.com>
2023-02-13 11:23:55 -0300

    felicidade

--- Nhash.py
+++ Nhash.py
@@ -1,12 +1,12 @@
 from hashlib import sha256
 import numpy as np
 import wisard_lut
-import sys
 
 tabela = wisard_lut.tabelahash()
-quanthash = 2    #quantidade de hashs
+quanthash = 2     #quantidade de hashs    2 , 4  , 8
 teste = []
 linha = 0  #calcular tabela em caso de erros 
+
 for d in tabela:
     linha = linha + 1
     addr = [] #chaves
@@ -24,13 +24,15 @@ for d in tabela:
         quant = quant+1            #calcula numero de termos em cada tabela
 
     
-    n_bits = np.ceil(np.log2(quant))   # quantidade de bits a serem pegos
-    ax = int(np.ceil(n_bits/4))        # quantidade de caract a pegos na sha [2] FF = 256  ~ 8 bits
+    n_bits = int(np.ceil(np.log2(quant)))   # quantidade de bits a serem pegos
+    ax = int(np.ceil(n_bits/4))        # quantidade de caract a pegos na sha [2~3] FF = [256~4096] = [8~12] bits
     sha = [sha256(addr[i].encode()).hexdigest() for i in range(len(addr))]  #armazenando os sha256
-    ay = 2**(ax*4)  # quantidade de linhas 
-
+    
+    ay = int(np.ceil((2**(n_bits))/quanthash))  # quantidade de linhas 
+    ab = int(np.ceil(np.log2(ay)))    # quantidade de bits a pegar sha inteiro
     matriz = np.full((quanthash , ay, 2), -1)   # matriz 3d de hashs [-1]
 
+    
     
     for i in range (0,quant,1) :
         temp = sha[i]
@@ -40,45 +42,36 @@ for d in tabela:
     for i in range(0,quant,1): #transforma de string para decimal 
         hex_string = str(shain[i])
         shain[i] = int(hex_string, 16)
+        sha_bi = format(shain[i],'b')
+        shain[i] = int( sha_bi[-ab:], 2 )    
+       
         hex_string = str(shaout[i])
         shaout[i] = int(hex_string, 16)   
        
-
+    
     for i in range(0,quant,1):
         tabHash = 0
         for j in range(quanthash):
             if matriz [tabHash, shain[i] , 1] == -1 :
-                matriz[tabHash,shain[i]] = [outs[i],shaout[i]]
-                
+                matriz[tabHash,shain[i]] = [outs[i],shaout[i]]                
                 break
             else :
                 tabHash = tabHash + 1
     
-
-    """for i in range(quanthash):  #mtd 1 de imp
-        for j in range(ay):
-            for k in range(2):
-                print(matriz[i, j, k], end=' ')   
-            print()
-        print()
-
-    np.set_printoptions(threshold=sys.maxsize) #mtd 2 de imp
-    print(matriz)"""
-    
     numTeste = 0
     for i in range(0,quanthash,1):        
-        for j in range(0,quant,1):
+        for j in range(0,ay,1):
             if matriz [i, j , 1] != -1 :
                 numTeste = numTeste + 1
 
                
     teste.append(numTeste)            
-            
+    #print(n_bits ,quant , ay , quanthash) #numero de bits pegos / numero de termos / numero de hashs     
 print(teste)
 
 
 
 
-
-    #print(ay,linha)  #verificador de tabela em caso de erro
+    
+    #print(ay,linha)  #verificador de tabela em caso de erro"""
 
