8e3b1eeabb3532b85dad67549954e1f5063959ab
erikemb <115750399+erikemb@users.noreply.github.com>
2023-02-27 11:12:07 -0300

    salve

--- Nhash.py
+++ Nhash.py
@@ -1,9 +1,10 @@
 from hashlib import sha256
 import numpy as np
 import wisard_lut
+import statistics
 
 tabela = wisard_lut.tabelahash()
-quanthash = 2     #quantidade de hashs    2 , 4  , 8
+quanthash = 1  #quantidade de hashs    1, 2 , 4  , 8 , 16
 teste = []
 linha = 0  #calcular tabela em caso de erros 
 
@@ -16,19 +17,21 @@ for d in tabela:
     shaout = [] #parte final da sha 
     quant = 0 #calcula numero de termos em cada tabela
     n_bits = 0 # quantidade de bits a serem pegos
-    
+    maior = 0
     for k in d.keys():     #carrega a tabela 
 
         addr.append(str(k))        #chaves
         outs.append(str(d[k]))     #valores
         quant = quant+1            #calcula numero de termos em cada tabela
+        if maior < k :
+            maior = k
 
     
-    n_bits = int(np.ceil(np.log2(quant)))   # quantidade de bits a serem pegos
+    n_bits = int(np.ceil(np.log2(maior)))   # quantidade de bits a serem pegos   
     ax = int(np.ceil(n_bits/4))        # quantidade de caract a pegos na sha [2~3] FF = [256~4096] = [8~12] bits
     sha = [sha256(addr[i].encode()).hexdigest() for i in range(len(addr))]  #armazenando os sha256
-    
-    ay = int(np.ceil((2**(n_bits))/quanthash))  # quantidade de linhas 
+    ar = int(np.ceil(np.log2(quant)))   #ver a exp pra base [2]^ar
+    ay = int(np.ceil((2**(ar))/quanthash))  # quantidade de linhas 
     ab = int(np.ceil(np.log2(ay)))    # quantidade de bits a pegar sha inteiro
     matriz = np.full((quanthash , ay, 2), -1)   # matriz 3d de hashs [-1]
 
@@ -65,10 +68,12 @@ for d in tabela:
                 numTeste = numTeste + 1
 
                
-    teste.append(numTeste)            
-    #print(n_bits ,quant , ay , quanthash) #numero de bits pegos / numero de termos / numero de hashs     
-print(teste)
-
+    teste.append((numTeste/quant)*100)           
+    #print(n_bits ,ax, quant , ay , quanthash) #numero de bits pegos / numero de termos / numero de hashs     
+media = np.mean(teste)
+print("Média:", media)
+desvio_padrao = statistics.stdev(teste)
+print(desvio_padrao)
 
 
 
